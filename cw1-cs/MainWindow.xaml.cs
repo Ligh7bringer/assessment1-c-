fb48fa4c0f39d02ab326104cd88ee1b65442c0f9
Svetlozar Georgiev <37ernity@gmail.com>
2016-10-24 22:38:19 +0100

    exception handling (and error mesages) + clear button functionality added

--- cw1-cs/MainWindow.xaml.cs
+++ cw1-cs/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ using System.Windows.Shapes;
 * Author: Svetlozar Georgiev, 40203970                                          *
 * Date of last change: 08/10/2017                                               *
 * Main window                                                                   *
-* Handles adding of attendees, gettin certificates and invoices                 *
+* Handles adding of attendees, getting certificates and invoices                *
 ********************************************************************************/
 
 namespace cw1_cs
@@ -29,5 +29,83 @@ namespace cw1_cs
             InitializeComponent();
         }
 
+        Attendee attendee1 = new Attendee();
+        private void btn_set_Click(object sender, RoutedEventArgs e)
+        {
+            //create new attendee object and set the parameters to user input
+            try
+            {
+                attendee1.AttendeeRef = Int32.Parse(txtbox_ref.Text);
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            try
+            {
+                attendee1.FirstName = txtbox_firstn.Text;
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            try
+            {
+                attendee1.SecondName = txtbox_lastn.Text;
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            attendee1.InstitutionName = txtbox_institution.Text;
+
+            try
+            {
+                attendee1.ConferenceName = txtbox_conference.Text;
+
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            attendee1.Paid = false;
+            if(cbox_paid.IsChecked.Value)
+            {
+                attendee1.Paid = true;
+            }
+            attendee1.Presenter = false;
+            if (cbox_presenter.IsChecked.Value)
+            {
+                attendee1.Presenter = true;
+            }
+
+            try
+            {
+                attendee1.PaperTitle = txtbox_paper.Text;
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void btn_clear_Click(object sender, RoutedEventArgs e)
+        {
+            txtbox_firstn.Clear();
+            txtbox_lastn.Clear();
+            txtbox_ref.Clear();
+            txtbox_institution.Clear();
+            txtbox_conference.Clear();
+            txtbox_regtype.Clear();
+            txtbox_paper.Clear();
+            cbox_paid.IsChecked = false;
+            cbox_presenter.IsChecked = false;
+
+        }
     }
 }
